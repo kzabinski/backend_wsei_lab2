5a21f559084d723e99613a3d858a030def1fb894
Krzysztof Żabiński <krzysztofzabinski@Krzysztofs-MacBook-Air.local>
2025-03-14 13:38:26 +0100

    Update to GetMapping

--- src/main/java/com/kzabinski/bakcend_wsei_lab1/BakcendWseiLab1Application.java
+++ src/main/java/com/kzabinski/bakcend_wsei_lab1/BakcendWseiLab1Application.java
@@ -2,16 +2,13 @@ package com.kzabinski.bakcend_wsei_lab1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
 public class BakcendWseiLab1Application {
 
-	@RequestMapping("/hello")
+	@GetMapping("/hello")
 	String home() {
 		return "Hello World!";
 	}
